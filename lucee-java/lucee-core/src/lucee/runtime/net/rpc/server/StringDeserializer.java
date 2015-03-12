22e867520a3c7f2392456ec30332ece062f1c673
miof <michael@lucee.org>
2015-03-12 17:07:03 +0100

    https://bitbucket.org/lucee/lucee/issue/210

--- lucee-java/lucee-core/src/lucee/runtime/net/rpc/server/StringDeserializer.java
+++ lucee-java/lucee-core/src/lucee/runtime/net/rpc/server/StringDeserializer.java
@@ -27,20 +27,20 @@ import javax.xml.namespace.QName;
 import org.apache.axis.encoding.ser.SimpleDeserializer;
 
 public class StringDeserializer extends SimpleDeserializer {
-	private static final Map<Pattern, String> replacements;
+	/*private static final Map<Pattern, String> replacements;
 	static {
 		replacements = new HashMap<Pattern, String>();
 		for (char c = 0x00; c <= 0x1F; ++c) {
 			replacements.put(Pattern.compile(StringSerializer.xmlCodeForChar(c)), Character.toString(c));
 		}
 		replacements.put(Pattern.compile(StringSerializer.xmlCodeForChar((char) 0x7F)), Character.toString((char) 0x7F));
-	}
+	}*/
 
 	public StringDeserializer(Class javaType, QName xmlType) {
 		super(javaType, xmlType);
 	}
 
-	public Object makeValue(String source) throws Exception {
+	/*public Object makeValue(String source) throws Exception {
 		String val = source; 
 
 		for (Pattern pattern : replacements.keySet()) {
@@ -48,5 +48,5 @@ public class StringDeserializer extends SimpleDeserializer {
 		}
 
 		return val; 
-	}
+	}*/
 }
--- lucee-java/lucee-core/src/lucee/runtime/net/rpc/server/StringSerializer.java
+++ lucee-java/lucee-core/src/lucee/runtime/net/rpc/server/StringSerializer.java
@@ -25,24 +25,25 @@ import org.apache.axis.encoding.ser.SimpleSerializer;
 import org.apache.commons.lang.StringUtils;
 
 public class StringSerializer extends SimpleSerializer {
-	protected static String xmlCodeForChar(char c) {
+	/*protected static String xmlCodeForChar(char c) {
 		StringBuilder buff = new StringBuilder();
 		buff.append("&#x");
 		buff.append(StringUtils.leftPad(Integer.toHexString(c), 4, "0"));
 		buff.append(";"); 
 		
 		return buff.toString();
-	}
+	}*/
 
-    public StringSerializer(Class javaType, QName xmlType) {
+	public StringSerializer(Class javaType, QName xmlType) {
 		super(javaType, xmlType);
 	}
 
 	public String getValueAsString(Object value, SerializationContext context) {
-        return escapeNonPrintableChars(super.getValueAsString(value, context));
-    }
+		//return escapeNonPrintableChars(super.getValueAsString(value, context));
+		return super.getValueAsString(value, context);
+	}
 
-	private String escapeNonPrintableChars(String val) {
+	/*private String escapeNonPrintableChars(String val) {
 		StringBuilder buff = new StringBuilder();
 		for (int idx = 0; idx < val.length(); ++idx) {
 			char c = val.charAt(idx);
@@ -55,5 +56,5 @@ public class StringSerializer extends SimpleSerializer {
 	private boolean charIsNonPrintable(char c) {
 		//0x00 to 0x1F and 0x7F are ASCII control characters
 		return c < 0x1F || c == 0x7F;
-	}
+	}*/
 }
