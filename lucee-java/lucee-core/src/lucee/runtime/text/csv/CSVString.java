f68b16e57dece76d2cbde1b04a9f5fef3f65f82a
michaeloffner <michael@rasia.ch>
2016-03-19 00:14:46 +0100

    improve fix for https://luceeserver.atlassian.net/browse/LDEV-778

--- lucee-java/lucee-core/src/lucee/runtime/text/csv/CSVString.java
+++ lucee-java/lucee-core/src/lucee/runtime/text/csv/CSVString.java
@@ -34,8 +34,7 @@ public class CSVString {
     private char delim;
 
     public CSVString( String input, char delim ) {
-
-        this.buffer = input.trim().toCharArray();
+        this.buffer = input.toCharArray();
         this.delim = delim;
     }
 
@@ -53,9 +52,7 @@ public class CSVString {
         do {
 
             c = buffer[ pos ];
-
             if ( c == '"' || c == '\'' ) {
-
                 sb.append( fwdQuote( c ) );
             }
             else if ( c == LF || c == CR ) {
@@ -75,11 +72,10 @@ public class CSVString {
                 sb.append( c );
 
             next();
-        } while ( hasNext() );
+        } while ( pos < buffer.length);
 
-        if(buffer.length>pos)sb.append( buffer[ pos ] );
         
-        line.add( sb.toString().trim() );
+        line.add( sb.toString() );
 
         if ( isValidLine( line ) )
             result.add( line );
