8bc60f7f5f784bef66c2ab65606c7e3085e91fbb
Dominic Watson <watson.dominic@gmail.com>
2017-06-23 13:24:27 +0100

    LDEV-1391 make connection NULL after closing it to avoid endless while loop
    Issue being that, once the connection stack is empty, the condition that checks for the connection being not null or invalid will always be true because the connection is not null and not valid (it is closed)

--- lucee-java/lucee-core/src/lucee/runtime/db/DatasourceConnectionPool.java
+++ lucee-java/lucee-core/src/lucee/runtime/db/DatasourceConnectionPool.java
@@ -4,17 +4,17 @@
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either 
+ * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
+ *
+ * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  **/
 package lucee.runtime.db;
 
@@ -39,10 +39,10 @@ import lucee.runtime.op.Caster;
 import lucee.runtime.type.util.ArrayUtil;
 
 public class DatasourceConnectionPool {
-	
+
 	private ConcurrentHashMap<String,DCStack> dcs=new ConcurrentHashMap<String,DCStack>();
 	private Map<String,RefInteger> counter=new ConcurrentHashMap<String,RefInteger>();
-	
+
 	public DatasourceConnection getDatasourceConnection(DataSource datasource, String user, String pass) throws PageException {
 		// pc=ThreadLocalPageContext.get(pc);
 		if(StringUtil.isEmpty(user)) {
@@ -50,33 +50,34 @@ public class DatasourceConnectionPool {
             pass=datasource.getPassword();
         }
         if(pass==null)pass="";
-		
+
 		// get stack
 		DCStack stack=getDCStack(datasource,user,pass);
-		
-		
+
+
 		// max connection
 		int max=datasource.getConnectionLimit();
-		
+
 		// get an existing connection
 		DatasourceConnectionImpl rtn=null;
 		do {
 			// we have a bad connection
 			if(rtn!=null) {
 				IOUtil.closeEL(rtn.getConnection());
+				rtn=null;
 			}
 			synchronized (stack) {
 				while(max!=-1 && max<=_size(datasource)) {
 					try {
 						//stack.inc();
 						stack.wait(10000L);
-						
-					} 
+
+					}
 					catch (InterruptedException e) {
 						throw Caster.toPageException(e);
 					}
 				}
-				
+
 				while(!stack.isEmpty()) {
 					DatasourceConnectionImpl dc=(DatasourceConnectionImpl) stack.get();
 					if(dc!=null){
@@ -86,11 +87,11 @@ public class DatasourceConnectionPool {
 				}
 			}
 		}while(rtn!=null && !isValid(rtn,Boolean.TRUE));
-		
+
 		// create a new connection
 		if(rtn==null)
 			rtn=loadDatasourceConnection(datasource, user, pass);
-		
+
 		synchronized (stack) {
 			_inc(datasource);
 		}
@@ -103,7 +104,7 @@ public class DatasourceConnectionPool {
         try {
         	conn = DBUtil.getConnection(connStr, user, pass);
         	conn.setAutoCommit(true);
-        } 
+        }
         catch (SQLException e) {
         	throw new DatabaseException(e,null);
         }
@@ -121,11 +122,11 @@ public class DatasourceConnectionPool {
 		//if(async)((SpoolerEngineImpl)config.getSpoolerEngine()).add((DatasourceConnectionImpl)dc);
 		//else releaseDatasourceConnection(dc);
 	}
-	
+
 	public void releaseDatasourceConnection(DatasourceConnection dc) {
 		releaseDatasourceConnection(dc, false);
 	}
-	
+
 	public void releaseDatasourceConnection(DatasourceConnection dc, boolean closeIt) {
 		if(dc==null) return;
 		DCStack stack=getDCStack(dc.getDatasource(), dc.getUsername(), dc.getPassword());
@@ -137,7 +138,7 @@ public class DatasourceConnectionPool {
 			if(max!=-1) {
 				_dec(dc.getDatasource());
 				stack.notify();
-				 
+
 			}
 			else _dec(dc.getDatasource());
 		}
@@ -145,7 +146,7 @@ public class DatasourceConnectionPool {
 
 	public void clear() {
 		//int size=0;
-		
+
 		// remove all timed out conns
 		try{
 			Object[] arr = dcs.entrySet().toArray();
@@ -171,19 +172,19 @@ public class DatasourceConnectionPool {
 				conns.clear();
         	}
 		}
-        
+
         RefInteger ri=counter.get(id);
 		if(ri!=null)ri.setValue(0);
 		else counter.put(id,new RefIntegerSync(0));
-        
-	}
-	
 
-	
+	}
+
+
+
 	public static boolean isValid(DatasourceConnection dc,Boolean autoCommit) {
 		try {
 			if(dc.getConnection().isClosed())return false;
-		} 
+		}
 		catch (Throwable t) {
         	ExceptionUtil.rethrowIfNecessary(t);
         	return false;
@@ -191,21 +192,21 @@ public class DatasourceConnectionPool {
 
 		try {
 			if(dc.getDatasource().validate() && !DataSourceUtil.isValid(dc,10))return false;
-		} 
+		}
 		catch (Throwable t) {
         	ExceptionUtil.rethrowIfNecessary(t);
         } // not all driver support this, because of that we ignore a error here, also protect from java 5
-		
-		
+
+
 		try {
 			if(autoCommit!=null) dc.getConnection().setAutoCommit(autoCommit.booleanValue());
-		} 
+		}
 		catch (Throwable t) {
         	ExceptionUtil.rethrowIfNecessary(t);
         	return false;
         }
-		
-		
+
+
 		return true;
 	}
 
@@ -214,14 +215,14 @@ public class DatasourceConnectionPool {
 		String id = createId(datasource,user,pass);
 		synchronized(id) {
 			DCStack stack=dcs.get(id);
-		
+
 			if(stack==null){
 				dcs.put(id, stack=new DCStack());
 			}
 			return stack;
 		}
 	}
-	
+
 	public int openConnections() {
 		Iterator<DCStack> it = dcs.values().iterator();
 		int count=0;
