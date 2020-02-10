3a79cd6ca99b59b836c9aae7f16b5067d5faa752
Kyle Carter <kylec32@gmail.com>
2020-02-10 12:12:17 -0700

    D-10646 - Don't ever kill tomcat threads

--- lucee-java/lucee-core/src/lucee/commons/io/SystemUtil.java
+++ lucee-java/lucee-core/src/lucee/commons/io/SystemUtil.java
@@ -1084,6 +1084,11 @@ class StopThread extends Thread {
 	}
 
 	public void run(){
+	    if (thread.getName().startsWith("mc-")) {
+            LogUtil.log(log, Log.LEVEL_ERROR, "", "Trying to stop a tomcat thread. This action is being skipped", thread.getStackTrace());
+            return;
+        }
+
 		if(pc!=null){
 			PageContextImpl pci=(PageContextImpl) pc;
 			pci.stop(t);
